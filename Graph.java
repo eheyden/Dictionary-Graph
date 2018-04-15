0e03c449f48ad6e37df4601d6287e598388fee52
eheyden <37028782+eheyden@users.noreply.github.com>
2018-04-14 20:36:13 -0500

    Fixed major Graph issues

--- Graph.java
+++ Graph.java
@@ -10,34 +10,39 @@ import java.util.LinkedList;
  */
 public class Graph<E> implements GraphADT<E> {
     
-	private LinkedList<GraphNode<E>> nodes;
-	
-	class Graphnode<E>{
-		private E data;
-		public LinkedList<Graphnode<E>> neighbors;
-		public Graphnode(E data) {
-		public LinkedList<GraphNode<E>> neighbors;
-		public GraphNode(E data) {
-			neighbors = new LinkedList<GraphNode<E>>();
-			this.data = data;
-		}
-		
-	}
-	
-	public Graph() {
-		nodes = new LinkedList<GraphNode<E>>;
-		
-	}
+    private LinkedList<GraphNode<E>> nodes;
+    
+    class GraphNode<E>{
+        private E data;
+        public LinkedList<GraphNode<E>> neighbors;
+        public GraphNode(E data) {
+            neighbors = new LinkedList<GraphNode<E>>();
+            this.data = data;
+        }
+        public E getData() {return data;}
+        
+    }
+    
+    public Graph() {
+        nodes = new LinkedList<GraphNode<E>>();
+        
+    }
     /**
      * Instance variables and constructors
      */
 
+    private GraphNode<E> getNode(E data){
+        for(GraphNode<E> node : nodes) {
+            if(node.getData().equals(data)) return node;
+        }
+        return null;
+    }
     /**
      * {@inheritDoc}
      */
     @Override
     public E addVertex(E vertex) {
-    	if(vertex == null || nodes.contains(vertex)) return null;
+        if(vertex == null || nodes.contains(vertex)) return null;
         GraphNode<E> node = new GraphNode<E>(vertex);
         nodes.add(node);
         return vertex;
@@ -48,13 +53,14 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public E removeVertex(E vertex) {
-    	if(vertex == null || nodes.contains(vertex)) return null;
-    	for(GraphNode<E> neighbor | vertex.neighbors) {
-    		removeEdge(neighbor, vertex);
-    	}
-    	nodes.remove(vertex);
-    	return vertex;
-    	
+        GraphNode<E> node = getNode(vertex);
+        if(vertex == null || node == null) return null;
+        for(GraphNode<E> neighbor : node.neighbors) {
+            removeEdge(neighbor.getData(), vertex);
+        }
+        nodes.remove(node);
+        return vertex;
+        
     }
 
     /**
@@ -64,10 +70,13 @@ public class Graph<E> implements GraphADT<E> {
     public boolean addEdge(E vertex1, E vertex2) {
         if(vertex1 == null || vertex2 == null) return false;
         if(vertex1.equals(vertex2)) return false;
-        if(!nodes.contains(vertex1) || !nodes.contains(vertex2)) return false;
-        if(vertex1.neighbors.contains(vertex2)) return false;
-        vertex2.neighbors.add(vertex1);
-        vertex1.neighbors.add(vertex2);
+        
+        GraphNode<E> node1 = getNode(vertex1);
+        GraphNode<E> node2 = getNode(vertex2);
+        if(node1 == null || node2 == null) return false;
+        if(node1.neighbors.contains(node2)) return false;
+        node2.neighbors.add(node1);
+        node1.neighbors.add(node2);
         return true;
     }    
 
@@ -77,11 +86,14 @@ public class Graph<E> implements GraphADT<E> {
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
         if(vertex1 == null || vertex2 == null) return false;
-        if(vertex1.equals(vertex2) return false;
-        if(!nodes.contains(vertex1) || !nodes.contains(vertex2)) return false;
-        if(!vertex2.neighbors.contains(vertex1)) return false;
-        vertex2.neighbors.remove(vertex1);
-        vertex1.neighbors.remove(vertex2);
+        if(vertex1.equals(vertex2)) return false;
+        
+        GraphNode<E> node1 = getNode(vertex1);
+        GraphNode<E> node2 = getNode(vertex2);
+        if(node1 == null || node2 == null) return false;
+        if(!node2.neighbors.contains(node1)) return false;
+        node2.neighbors.remove(node1);
+        node1.neighbors.remove(node2);
         return true;
     }
 
@@ -90,10 +102,13 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
-    	if(vertex1 == null || vertex2 == null) return false;
-        if(vertex1.equals(vertex2) return false;
-        if(!nodes.contains(vertex1) || !nodes.contains(vertex2)) return false;
-        if(vertex1.neighbors.contains(vertex2)) return true;
+        if(vertex1 == null || vertex2 == null) return false;
+        if(vertex1.equals(vertex2)) return false;
+        
+        GraphNode<E> node1 = getNode(vertex1);
+        GraphNode<E> node2 = getNode(vertex2);
+        if(node1 == null || node2 == null) return false;
+        if(node1.neighbors.contains(node2)) return true;
         return false;
     }
 
@@ -103,8 +118,10 @@ public class Graph<E> implements GraphADT<E> {
     @Override
     public Iterable<E> getNeighbors(E vertex) {
         if(vertex == null) return null;
-        if(!nodes.contains(vertex)) return null;
-        return vertex.neighbors;
+        
+        GraphNode<E> node = getNode(vertex);
+        if(node == null) return null;
+        return (Iterable<E>) node.neighbors;
     }
 
     /**
@@ -112,7 +129,7 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getAllVertices() {
-        return nodes;
+        return (Iterable<E>) nodes;
     }
 
 }
